4cd0e4ffaecd6359ad0444445c83361f011ebdb5
shreyaswaghmare0210 <shreyaswaghmare0210@gmail.com>
2026-02-03 14:29:39 +0530

    Update

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// Remove hash to prevent footer jump
+window.addEventListener("load", () => {
+  if (window.location.hash) {
+    history.replaceState(null, null, window.location.pathname);
+  }
+  window.scrollTo(0, 0);
+});
+
+
+// Always start page from top on reload
+window.history.scrollRestoration = "manual";
+
+window.addEventListener("beforeunload", () => {
+  window.scrollTo(0, 0);
+});
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+
+  const loader = document.getElementById("loader");
+  setTimeout(() => {
+    loader.classList.add("fade-out");
+  }, 800);
+});
+
+
 const mobileMenu = document.getElementById('mobile-menu');
         const navList = document.getElementById('nav-list');
 
@@ -10,3 +40,38 @@ const mobileMenu = document.getElementById('mobile-menu');
                 navList.classList.remove('active');
             });
         });
+
+        const text = "Shreyas Waghmare";
+let index = 0;
+const speed = 100;
+const typingElement = document.getElementById("typing");
+
+function typeEffect() {
+  if (index < text.length) {
+    typingElement.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, speed);
+  }
+}
+
+typeEffect();
+
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    if (elementTop < windowHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+});
+
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.classList.add("fade-out");
+  }, 800); // delay for smooth feel
+});
